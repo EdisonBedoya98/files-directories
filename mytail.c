9f05ef503aa5008f80937055727e1f10a9de589c
deyberst <deyberst@gmail.com>
2019-10-07 15:50:27 -0500

    Subiendo punto 3 terminado

--- mytail.c
+++ mytail.c
@@ -8,24 +8,49 @@
 int main(int argc, char *argv[])
 {
     int n = atoi(argv[1]);
-
     printf("number: %d\n", n);
-    char cwd[256];
-    if (getcwd(cwd, sizeof(cwd)) != NULL)
+
+    char ch, file_name[15];
+    FILE *fp;
+
+    fp = fopen(argv[2], "r"); // read mode
+
+    if (fp == NULL)
     {
-        const size_t len1 = strlen(cwd);
-        const size_t len2 = strlen(argv[2]);
-        char *result = malloc(len1 + len2 + 1);
-
-        memcpy(result, cwd, len1);
-        memcpy(result + len1, argv[2], len2 + 1);
-
-        printf("Current worjing directory is: %s\n", result);
-        FILE *fptr;
-        fopen(result, "r");
-
-        fclose(fptr);
-
-        return 0;
+        perror("Error while opening the file.\n");
+        exit(EXIT_FAILURE);
     }
+
+    int contador = 0;
+    int contador2 = 0;
+
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    // while para contar el número de lineas que tiene el archivo
+    while ((read = getline(&line, &len, fp)) != -1)
+    {
+        contador++;
+    }
+
+    // le restamos al contador el número de lineas al final que se quieren imprimir para saber donde iniciar
+    int inicio = contador + n;
+
+    // reiniciamos el apuntador que abre el archivo
+    fp = fopen(argv[2], "r");
+
+    //while para imprimir las lineas solicitadas
+    while ((read = getline(&line, &len, fp)) != -1)
+    {
+        contador2++;
+        if (contador2 > inicio)
+        {
+            printf("%s", line);
+        }
+    }
+
+    //cerramos el archivo
+    fclose(fp);
+    return 0;
 }
